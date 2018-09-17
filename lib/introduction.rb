167ea0e47ecf364dc3f75d236106833180856150
Roman Pirag <mr.anadale@yahoo.com>
2018-09-17 19:22:56 +0000

    automatically backed up by learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,7 +1,7 @@
 
-def #introduction(name,language)
+def introduction(name,language)
   puts "Hello my name is #{name}" + " " + "and I speak #{language}"
 end
 
 
-#introduction("John","English")
+introduction("John","English")
